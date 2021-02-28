576a686085890adf8051d77bd82d6fe58f318fc2
Hunter Kreshock <hkreshock@gmail.com>
2021-02-28 09:50:06 -0800

    updated outputs and added test strings with \n included

---
+++ code/google/testStrings/a.js
@@ -0,0 +1,1 @@
+const test = '6 4 5 2 1000\na2 0 rue-de-londres 1\na0 1 rue-d-amsterdam 1\na3 1 rue-d-athenes 1\na2 3 rue-de-rome 2\na1 2 rue-de-moscou 3\na4 rue-de-londres rue-d-amsterdam rue-de-moscou rue-de-rome\na3 rue-d-athenes rue-de-moscou rue-de-londres';
